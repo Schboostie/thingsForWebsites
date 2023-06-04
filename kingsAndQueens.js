40b5268a420db698ca86e141559edf10eca26403
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-06-04 10:52:06 +0100

    Create kingsAndQueens.js

---
+++ kingsAndQueens.js
@@ -0,0 +1,57 @@
+const monarchs = {
+  HenryVIII: {
+    name: 'Henry VIII',
+    reign: '1509-1547',
+    fact: 'Henry VIII had six wives and is known for the English Reformation.'
+  },
+  ElizabethI: {
+    name: 'Elizabeth I',
+    reign: '1558-1603',
+    fact: 'Elizabeth I was the last monarch of the Tudor dynasty and is considered one of England\'s greatest rulers.'
+  },
+  Victoria: {
+    name: 'Queen Victoria',
+    reign: '1837-1901',
+    fact: 'Queen Victoria was the longest-reigning monarch in British history and the queen during the height of the British Empire.'
+  },
+  RichardIII: {
+    name: 'Richard III',
+    reign: '1483-1485',
+    fact: 'Richard III is known for his controversial rise to power and his defeat at the Battle of Bosworth, which marked the end of the Plantagenet dynasty.'
+  },
+  EdwardIII: {
+    name: 'Edward III',
+    reign: '1327-1377',
+    fact: 'Edward III reigned during the Hundred Years\' War and was known for his military successes against France.'
+  },
+  MaryI: {
+    name: 'Mary I',
+    reign: '1553-1558',
+    fact: 'Mary I, also known as "Bloody Mary," attempted to reverse the English Reformation and restore Catholicism in England.'
+  },
+  WilliamI: {
+    name: 'William I (William the Conqueror)',
+    reign: '1066-1087',
+    fact: 'William I led the Norman Conquest of England and introduced Norman customs and feudalism to the country.'
+  },
+  CharlesII: {
+    name: 'Charles II',
+    reign: '1660-1685',
+    fact: 'Charles II was restored to the throne after the English Civil War and was known for his extravagant lifestyle and patronage of the arts.'
+  },
+  Anne: {
+    name: 'Queen Anne',
+    reign: '1702-1714',
+    fact: 'Queen Anne was the last monarch of the Stuart dynasty and the first monarch of Great Britain after the Act of Union in 1707.'
+  },
+  GeorgeIII: {
+    name: 'George III',
+    reign: '1760-1820',
+    fact: 'George III was the king during the American Revolutionary War and is known for his struggles with mental illness.'
+  }
+};
+
+// Accessing the information for a specific monarch
+console.log(monarchs.HenryVIII.name); // Output: 'Henry VIII'
+console.log(monarchs.ElizabethI.reign); // Output: '1558-1603'
+console.log(monarchs.Victoria.fact); // Output: 'Queen Victoria was the longest-reigning monarch in British history and the queen during the height of the British Empire.'
